de62a92a7bacb59d94ba6646ae164aa098d77b09
LeeMin9yeon9 <122258997+LeeMin9yeon9@users.noreply.github.com>
2025-11-18 16:15:24 +0900

    [2025-11-18 / 이민경 / Spring Boot CORS 설정]

--- src/main/java/kr/co/winnticket/WinticketApplication.java
+++ src/main/java/kr/co/winnticket/WinticketApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "kr.co.winnticket")
 public class WinticketApplication {
 
     public static void main(String[] args) {
