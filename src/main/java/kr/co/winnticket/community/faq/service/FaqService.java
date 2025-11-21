6bff656dc8281dab5cdf32ccd369c588d0231da7
LeeMin9yeon9 <122258997+LeeMin9yeon9@users.noreply.github.com>
2025-11-21 10:24:41 +0900

    Update FaqService.java

--- src/main/java/kr/co/winnticket/community/faq/service/FaqService.java
+++ src/main/java/kr/co/winnticket/community/faq/service/FaqService.java
@@ -54,12 +54,12 @@ public class FaqService {
     }
 
     // 카테고리 수정
-    public void updateFaqCategory(String asId, FaqCategoryPatchReqDto model) {
-        mapper.updateFaqCategory(asId, model.getName());
+    public void updateFaqCategory(UUID auId, FaqCategoryPatchReqDto model) {
+        mapper.updateFaqCategory(auId, model.getName());
     }
 
     // 카테고리 삭제
-    public void deleteFaqCategory(String asId) {
-        mapper.deleteFaqCategory(asId);
+    public void deleteFaqCategory(UUID auId) {
+        mapper.deleteFaqCategory(auId);
     }
 }
