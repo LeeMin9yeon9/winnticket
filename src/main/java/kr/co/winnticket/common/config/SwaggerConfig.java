9f528b01b4446165d1b150f3c9a7117116b3b4ca
이민경 <dlalsrud0624@naver.com>
2025-11-27 00:34:15 +0900

    [2025 - 11- 27 / 이민경 / 스웨거오류조치 ]

--- src/main/java/kr/co/winnticket/common/config/SwaggerConfig.java
+++ src/main/java/kr/co/winnticket/common/config/SwaggerConfig.java
@@ -2,17 +2,47 @@ package kr.co.winnticket.common.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${server.port:8080}")
+    private String port;
+
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI openAPI(Environment environment) {
+
+        String[] profiles = environment.getActiveProfiles();
+        String activeProfile = (profiles.length > 0) ? profiles[0] : "local";
+
+        // profile 별 서버 주소 설정
+        String serverUrl;
+        String description;
+
+        switch (activeProfile) {
+            case "dev":
+                serverUrl = "https://api.winnticket.store";
+                description = "DEV Server";
+                break;
+            default:
+                serverUrl = "http://localhost:" + port;
+                description = "LOCAL Server";
+        }
+
         return new OpenAPI()
-            .info(new Info()
-                    .title("윈티켓 API 문서")                          // 문서 제목
-                    .version("v1.0.0")                               // 버전
-                    .description("윈티켓 프로젝트의 API 명세 문서입니다."));  // 설명
+                .info(new Info()
+                        .title("WinnTicket API Docs")
+                        .description("윈앤티켓 백엔드 API 명세서")
+                        .version("v1.0"))
+                .servers(List.of(
+                        new Server().url(serverUrl).description(description)
+                ));
     }
 }
