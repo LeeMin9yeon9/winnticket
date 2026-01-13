460ae7dc93ccb186e3fd871436d80d14220013cb
ellen <ellen@keystonesec.com>
2026-01-13 23:04:17 +0900

    [2026-01-13/김한빛/베네피아복호화테스트 수정]

--- src/main/java/kr/co/winnticket/integration/benepia/crypto/BenepiaSeedEcbCrypto.java
+++ src/main/java/kr/co/winnticket/integration/benepia/crypto/BenepiaSeedEcbCrypto.java
@@ -3,6 +3,7 @@ package kr.co.winnticket.integration.benepia.crypto;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Component;
 
+import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
@@ -10,14 +11,19 @@ import java.util.Base64;
 @Tag(name = "베네피아", description = "SEED(ECB Crypto)")
 public class BenepiaSeedEcbCrypto {
 
-    public String decrypt(String encParamBase64,String seedKey){
+    public String decrypt(String encParam,String seedKey){
         try{
-            byte[] cipher = Base64.getDecoder().decode(encParamBase64);
-            byte[] key = seedKey.getBytes(StandardCharsets.ISO_8859_1);
+            String urlDecoded = URLDecoder.decode(encParam,StandardCharsets.UTF_8);
 
+            // Base64 디코딩
+            byte[] cipher = Base64.getDecoder().decode(urlDecoded);
+
+            // SEED ECB 복호화
+            byte[] key = seedKey.getBytes(StandardCharsets.ISO_8859_1);
             byte[] plain = KISA_SEED_ECB.SEED_ECB_Decrypt(key,cipher,0,cipher.length);
 
             return new String(plain, StandardCharsets.UTF_8).trim();
+
         } catch(Exception e){
             throw new IllegalStateException("Benepia SEED-ECB decrypt 실패", e);
         }
