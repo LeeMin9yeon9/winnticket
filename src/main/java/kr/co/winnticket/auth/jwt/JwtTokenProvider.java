1a5ca41fad7f33eca5b8c28fb8410a27e05c6e87
ellen <ellen@keystonesec.com>
2026-02-04 14:06:08 +0900

    no message

--- src/main/java/kr/co/winnticket/auth/jwt/JwtTokenProvider.java
+++ src/main/java/kr/co/winnticket/auth/jwt/JwtTokenProvider.java
@@ -4,12 +4,12 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
@@ -25,7 +25,8 @@ public class JwtTokenProvider {
             @Value("${jwt.refresh-token-validity}") long refreshTokenValidMs
     ) {
          System.out.println("=== SERVER SECRET ===");
-        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secret));
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+       // this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secret));
         this.accessTokenValidMs = accessTokenValidMs;
         this.refreshTokenValidMs = refreshTokenValidMs;
     }
