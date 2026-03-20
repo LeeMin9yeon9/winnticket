3f26eeec9e1880547d7c5158c88e48679b8731eb
ellen <ellen@keystonesec.com>
2026-03-21 01:21:16 +0900

    [2026-03-21/김한빛/베네피아 채널 api수정]

--- src/main/java/kr/co/winnticket/product/admin/mapper/ProductMapper.java
+++ src/main/java/kr/co/winnticket/product/admin/mapper/ProductMapper.java
@@ -291,4 +291,9 @@ public interface ProductMapper {
 
     // 입금 완료 시 옵션값 재고 차감
     void decreaseStock(@Param("optionValueId") UUID optionValueId);
+
+    // 메뉴 활비 시 상품 활/비
+    void updateProductsVisibleByMenuId(@Param("categoryId") UUID id,
+                                       @Param("visible") Boolean visible);
 }
+
