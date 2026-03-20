e0a67cb7dba93d1e845e3c04f9947f7bb66e3771
이민경 <dlalsrud0624@naver.com>
2026-03-21 00:37:21 +0900

    [2026-03-21 / 이민경 / 베네피아 orderService 수정, 쇼핑몰 주문조회시 채널의 주문만 보이도록 수정]

--- src/main/java/kr/co/winnticket/integration/benepia/order/dto/BenepiaOrderBatchRequest.java
+++ src/main/java/kr/co/winnticket/integration/benepia/order/dto/BenepiaOrderBatchRequest.java
@@ -7,6 +7,6 @@ import java.util.List;
 @Data
 public class BenepiaOrderBatchRequest {
     private String header = "H";   // 문서 고정값
-    private List<Object> body;     // 주문 + 취소
     private String tail = "T";     // 문서 고정값
+    private List<Object> body;     // 주문 + 취소
 }
